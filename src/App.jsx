c987cc981cd29c2b79850b428fed53786814f946
Куницкий Ростислав Борисович <kunitskiy.rb@cso.com>
2023-10-24 16:56:12 +0300

    header dekstop css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './components/header/header.jsx'
+import MainPage from './components/mainPage/mainPage.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <MainPage />
     </>
   )
 }
---
+++ src/components/mainPage/MainPage.jsx
@@ -0,0 +1,20 @@
+import './MainPage.css'
+
+export default function MainPage() {
+    return (
+        <div className="mainPage">
+            <div className='container'>
+                <section className='mainPage_mainPicture'>
+                    <div>
+                        <h1>сервис по поиску публикаций о компании по его ИНН</h1>
+                        <p>Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.</p>
+                        <button>Запросить данные</button>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}   
