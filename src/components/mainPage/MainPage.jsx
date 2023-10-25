3a3419e532c81a521ac33b09b554652a529666b3
Куницкий Ростислав Борисович <kunitskiy.rb@cso.com>
2023-10-25 11:24:19 +0300

    header dekstop css

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -4,16 +4,20 @@ export default function MainPage() {
     return (
         <div className="mainPage">
             <div className='container'>
-                <section className='mainPage_mainPicture'>
-                    <div>
-                        <h1>сервис по поиску публикаций о компании по его ИНН</h1>
-                        <p>Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.</p>
-                        <button>Запросить данные</button>
+                <section className='mainPage__mainPicture'>
+                    <div className='mainPage__mainPicture-text'>
+                        <h1 className='heading heading_h1'>сервис по поиску публикаций<br />о компании<br /> по его ИНН</h1>
+                        <p>Комплексный анализ публикаций, получение данных<br /> в формате PDF на электронную почту.</p>
+                        <button className='button button-standart'>Запросить данные</button>
                     </div>
-                    <div>
-                        <img src="" alt="" />
+                    <div className='mainPage__mainPicture-img'>
+                        <img src="/assets/main_main-picture.png" alt="Главная картинка" />
                     </div>
                 </section>
+                <section className='mainPage__carousel'>
+                        <h2 className='heading heading_mainPage-h2'>Почему именно мы</h2>
+
+                </section>  
             </div>
         </div>
     )
