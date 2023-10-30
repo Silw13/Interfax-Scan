080ad40f0e340fbe1600ace999d36a9247c4d6ea
Куницкий Ростислав Борисович <kunitskiy.rb@cso.com>
2023-10-30 17:03:56 +0300

    footer css dekstop

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import Header from './components/header/header.jsx'
 import MainPage from './components/mainPage/mainPage.jsx'
 import Footer from './components/footer/Footer.jsx'
+import AuthentificationPage from './components/authenticationPage/AuthentificationPage.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
-      <MainPage />
+      {/* <MainPage /> */}
+      <AuthentificationPage />
       <Footer />
     </>
   )
